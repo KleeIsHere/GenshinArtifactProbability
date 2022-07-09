8ff7904acfa5c57fb28a3a7ef29bdcf27d657bc3
KleeIsHere <kleegenshin1o1@gmail.com>
2022-07-09 16:51:34 -0600

    Fixed multipage app framework

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
-from dash import Dash, dcc, html, Input, Output, callback
-from pages import page1, page2
+import dash
+from dash import dcc, html, Output, Input, State
+import dash_labs as dl
+import dash_bootstrap_components as dbc
 
 
-app = Dash(__name__, suppress_callback_exceptions=True)
-server = app.server
+app = dash.Dash(
+    __name__, plugins=[dl.plugins.pages], external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
 
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
+offcanvas = html.Div(
+    [
+        dbc.Button("Explore", id="open-offcanvas", n_clicks=0),
+        dbc.Offcanvas(
+            dbc.ListGroup(
+                [
+                    dbc.ListGroupItem(page["name"], href=page["path"])
+                    for page in dash.page_registry.values()
+                    if page["module"] != "pages.not_found_404"
+                ]
+            ),
+            id="offcanvas",
+            is_open=False,
+        ),
+    ],
+    className="my-3"
+)
 
+app.layout = dbc.Container(
+    [offcanvas, dl.plugins.page_container],
+    fluid=True,
+)
 
-@callback(Output('page-content', 'children'),
-              Input('url', 'pathname'))
-def display_page(pathname):
-    if pathname == '/page1':
-        return page1.layout
-    elif pathname == '/page2':
-        return page2.layout
-    else:
-        return '404'
+@app.callback(
+    Output("offcanvas", "is_open"),
+    Input("open-offcanvas", "n_clicks"),
+    [State("offcanvas", "is_open")],
+)
+def toggle_offcanvas(n1, is_open):
+    if n1:
+        return not is_open
+    return is_open
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8001)    
